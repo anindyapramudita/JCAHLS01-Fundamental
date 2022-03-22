f77e0e9ee45d5296b8703db8da1bbe442245dbed
anindyapramudita <anindyapramudita2111@gmail.com>
2022-03-23 00:03:30 +0700

    market updated

--- Market/script.js
+++ Market/script.js
@@ -11,7 +11,7 @@ class Product {
 
 let dbProduct = [
     new Product("SKU-1-126374", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Oreo-Two-Cookies.png/1200px-Oreo-Two-Cookies.png", "Oreo", "Food", 25, 7500),
-    new Product("SKU-2-198374", "https://images.tokopedia.net/img/cache/500-square/hDjmkQ/2021/12/20/917461cb-13f8-46b1-bb80-34f35688a10f.jpg", "Pocari", "Food", 50, 10000)
+    new Product("SKU-2-198374", "https://images.tokopedia.net/img/cache/500-square/hDjmkQ/2021/12/20/917461cb-13f8-46b1-bb80-34f35688a10f.jpg", "Pocari", "Drink", 50, 10000)
 ];
 
 let selectedIndex = null;
@@ -24,26 +24,28 @@ function printProduct() {
             <td><img src="${value.img}" width = 100></td>
             <td><input type="text" id ="new-name" value="${value.name}"/></td>
             <td>${value.category}</td>
-            <td><input type="text" id ="new-name" value="${value.stock.toLocaleString()}"/></td>
-            <td><input type="text" id ="new-name" value="IDR ${value.price.toLocaleString()}"/></td>
-            <td><button type="button" onClick="handleSave()">Save</button>
+            <td><input type="text" id ="new-stock" value="${value.stock.toLocaleString()}"/></td>
+            <td><input type="text" id ="new-price" value="${value.price}"/></td>
+            <td><button type="button" onClick="handleSave(${index})">Save</button>
             <button type="button" onClick="handleCancel()">Cancel</button></td>
             </tr>`
-        } else {
+        }
+        else {
             return `<tr>
-        <td>${value.sku}</td>
-        <td><img src="${value.img}" width = 100></td>
-        <td>${value.name}</td>
-        <td>${value.category}</td>
-        <td>${value.stock.toLocaleString()}</td>
-        <td>IDR ${value.price.toLocaleString()}</td>
-        <td><button type="button" onClick="handleEdit(${index})">Edit</button>
-        <button type="button" onClick="handleDelete(${index})">Delete</button></td>
-        </tr>`
+            <td>${value.sku}</td>
+            <td><img src="${value.img}" width = 100></td>
+            <td>${value.name}</td>
+            <td>${value.category}</td>
+            <td>${value.stock.toLocaleString()}</td>
+            <td>IDR ${value.price.toLocaleString()}</td>
+            <td><button type="button" onClick="handleEdit(${index})">Edit</button>
+            <button type="button" onClick="handleDelete(${index})">Delete</button></td>
+            </tr>`
         }
     }).join("")
 
 }
+
 
 function handleSubmit() {
     let data = document.getElementById("form-product")
@@ -80,19 +82,58 @@ function handleDelete(index) {
 function handleEdit(index) {
     selectedIndex = index;
     printProduct();
+}
 
-    // let idProductName = "name-" + index;
-    // let idProductStock = "stock-" + index;
-    // let idProductPrice = "price-" + index;
-    // document.getElementById(idProductName).innerHTML = `<input type="text" />`; 
-    // document.getElementById(idProductStock).innerHTML = `<input type="number"></input>`;
-    // document.getElementById(idProductPrice).innerHTML = `<input type="number"></input>`; 
-    // document.getElementById(idProductName).innerHTML = `<input id="productName-${index}" type="text"></input>`;
-    // document.getElementById(idProductStock).innerHTML = `<input id="productStock-${index}" type="number"></input>`;
-    // document.getElementById(idProductPrice).innerHTML = `<input id="productPrice-${index}" type="number"></input>`;
-    // document.getElementById(idProductName).innerHTML = `<input id="productName-${index}" type="text" placeholder="${dbProduct[index].name"></input>`;
-    // document.getElementById(idProductStock).innerHTML = `<input id="productStock-${index}" type="number" placeholder="${parseInt(dbProduct[index].stock)}"></input>`;
-    // document.getElementById(idProductPrice).innerHTML = `<input id="productPrice-${index}" type="number" placeholder="${parseInt(dbProduct[index].price)}"></input>`;
+function handleSave(index) {
+    dbProduct[index].name = document.getElementById("new-name").value
+    dbProduct[index].stock = document.getElementById("new-stock").value
+    dbProduct[index].price = parseInt(document.getElementById("new-price").value)
+
+    selectedIndex = null;
+
+    printProduct();
+}
+
+function handleCancel() {
+    selectedIndex = null;
+
+    printProduct();
+}
+
+function handleSearch() {
+    let data = document.getElementById("form-filter")
+    let byName = data.elements[0].value
+    let byMin = data.elements[1].value
+    let byMax = data.elements[2].value
+    let byCategory = data.elements[3].value
+
+    let filter = dbProduct.filter((value, index) => {
+        return (value.name == byName || byName == "") && (value.category == byCategory || byCategory == "null") && (value.price >= parseInt(byMin) || byMin == "") && (value.price <= parseInt(byMax) || byMax == "");
+    })
+
+    document.getElementById("table-list").innerHTML = filter.map((value, index) => {
+        return `<tr>
+        <td>${value.sku}</td>
+        <td><img src="${value.img}" width = 100></td>
+        <td>${value.name}</td>
+        <td>${value.category}</td>
+        <td>${value.stock.toLocaleString()}</td>
+        <td>IDR ${value.price.toLocaleString()}</td>
+        <td><button type="button" onClick="handleEdit(${index})">Edit</button>
+        <button type="button" onClick="handleDelete(${index})">Delete</button></td>
+        </tr>`
+    }).join("")
+
+}
+
+function handleReset() {
+    let data = document.getElementById("form-filter")
+
+    data.elements[0].value = null;
+    data.elements[1].value = null;
+    data.elements[2].value = null;
+    data.elements[3].value = null;
+
     printProduct();
 }
 
